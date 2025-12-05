76471c7552cf1cce1fc4d6e0dfef934d47e0b675
Suryateja Challa <surtecha@gmail.com>
2025-12-05 23:02:34 +0530

    this is a mess. starting again

--- ui/components/progress_bar.py
+++
@@ -1,7 +0,0 @@
-from PySide6.QtWidgets import QProgressBar
-
-class ProgressBar(QProgressBar):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setRange(0, 100)
-        self.setValue(0)
