4abeb079bb07b040189b9a0590c6fe5eb0a4e15d
Suryateja Challa <surtecha@gmail.com>
2025-12-11 00:31:42 +0530

    feat: Add application name, organization, and window icon.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from app.app_controller import AppController
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setApplicationName("ORBITT")
+    app.setOrganizationName("ORBITT")
+    app.setWindowIcon(QIcon("assets/icon.png"))
     controller = AppController()
     controller.show()
     sys.exit(app.exec())
